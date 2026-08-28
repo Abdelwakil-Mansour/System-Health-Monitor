f7421b3ad3f7082e9f63d9386f9aa045cbf7a051
Abdelwakil Mansour <s-abdelwakil.abdelwakil@zewailcity.edu.eg>
2026-08-28 23:01:29 +0300

    Enhance system_health.sh with comments
    Added comments to clarify system health metrics.

--- system_health.sh
+++ system_health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "System Health Summary:"
 echo "------------------------"
-echo "CPU Load : $(uptime | awk -F'load average:' '{ print $2 }')"
-echo "Memory   : $(free -h | awk '/Mem:/ {print $3 "/" $2}')"
-echo "Disk     : $(df -h / | awk 'NR==2 {print $3 "/" $2}')"
+echo "CPU Load : $(uptime | awk -F'load average:' '{ print $2 }')" #Get the CPU Usaged
+echo "Memory   : $(free -h | awk '/Mem:/ {print $3 "/" $2}')"# Get the Memory Utilization
+echo "Disk     : $(df -h / | awk 'NR==2 {print $3 "/" $2}')" # Get the Disk Utilization
 echo "Uptime   : $(uptime -p)"
